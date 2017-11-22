ea7db9a22fe46ffe2f0f0eb923290a5d35baf57d
RRaffoul <rom.raffoul@gmail.com>
2017-11-22 22:36:50 +0100

    Voila avec une fonction en plus
    Qlq modifs, fini pour moi pour aujourd'hui

--- PlateauRomain.cpp
+++ PlateauRomain.cpp
@@ -51,20 +51,40 @@ bool Plateau::check_input(int x, int y){
 
 /*check si il y a un pion de la meme couleur plus loin dans cette direction, le 2e check pour manger quoi*/
 
-bool check_direction(int x, int y, int direction[2], int turn){
+bool check_direction(int x, int y, int direction[2]){
 
     //if(position + direction >= 0 && position + direction < 63){
     //int dist = 2; //distance entre les 2 pions enserrant les autres
     int dx = direction[0];
     int dy = direction[1]; //donne la direction en X et en Y dans laquelle on cherche
+    int color = (turn + 1)%2 + 1; //pour savoir si on est au tour du joueur 1 ou 2 sans avoir modulo d'un nombre paire = 0 mais = 2
 		for(int dist = 2, dist < 8, dist++){
 			if(((x + dist*dx) > 7) || ((x + dist*dx) <0) || ((y + dist*dy) > 7) || ((y + dist*dy) <0) || (plateau[x + dist*dx][y + dist*dy] == 0)) {
 				//check si on sort pas du plateau ou si on arrive pas sur une case vide
 				return false;
 			}
-			else if (plateau[x + dist*dx][y + dist*dy] = turn){
+			else if (plateau[x + dist*dx][y + dist*dy] = color){
 				return true;
 			}
 		} 
-	return false; //idée : retourner direct le nombre dist pour connaitre le nombre de pions a changer et leur positions
+	return false; //idée : retourner direct le nombre dist pour connaitre le nombre de pions a changer et leurs positions
 }
+
+int Plateau::check_eat(int x, int y){
+	int color = (turn +1)%2 + 1;
+	int count = 0;
+	int direction[8][2]={0};
+	for (int i = -1, i <=1, i++){
+		for (int j = -1, j <= 1, j++){//je sais que ca checke aussi la position meme du pion mais bon ca evite qlq lignes de codes pour une bete test
+			if (plateau[x + i][y + j] != 0 && plateau[x + i][y + j] != color){
+				/* alors la soit on bosse avec des vectors --> taille dimensionnable, comme pour checker qu'une direction apres
+				 * soit on bosse en tableau toujours avec d'office une taille de 8 fois 2 element pour avoir les 8 directions
+				 * avec des 0 quand il ne faut pas checker par la */
+				 direction[count][0] = i;
+				 direction[count][1] = j;
+			}
+			count++;
+		}
+	}
+	return direction;
+}/*maintenant encore faire une fonction qui goupille celle ci et check_direction en utilisant cette derniere que si la rangée de la matrice direction est != {0,0}
