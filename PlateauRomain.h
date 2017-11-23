bc02f11d4ceb132d75a4812e46192e940b0a06e5
RRaffoul <rom.raffoul@gmail.com>
2017-11-23 14:21:45 +0100

    Update PlateauRomain.h

--- PlateauRomain.h
+++ PlateauRomain.h
@@ -13,5 +13,12 @@ class Plateau {
 	int color;
  public :
 	Plateau ();
+	void print_board();
+	bool check_input(int x, int y);
+	bool check_direction(int x, int y, int direction[2]);
+	bool check_eat(int x, int y);
+	void eat();
+	bool check_notplay(int* board, int turn);
+	void player_turn();
 };
 
