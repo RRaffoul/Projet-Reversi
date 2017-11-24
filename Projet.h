122404bb4c305ccdd4a8630bc8c3235f2f588f41
RRaffoul <rom.raffoul@gmail.com>
2017-11-24 17:55:51 +0100

    C est le bon ca

---
+++ Projet.h
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+class Plateau {
+ private :
+	int plateau [8][8]; 
+	vector<int> pos_to_eat;
+	int turn = 0;
+	int color;
+	int passe = 0;
+	int noirs;
+	int blancs;
+ public :
+	Plateau ();
+	void print_board();
+	bool check_input(int x, int y);
+	bool check_direction(int x, int y, int direction[2]);
+	bool check_eat(int x, int y);
+	void eat();
+	bool check_notplay();
+	bool player_turn();
+};
+
