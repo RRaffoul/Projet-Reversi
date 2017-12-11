f1a2b58214fa34611409178333042fa8d1c3cc04
Louis Vande Perre <Louis.Vande.Perre@ulb.ac.be>
2017-12-11 20:48:51 +0100

    Modifications de fichiers .cpp uniquement, ça avance

--- fichiers.cpp
+++ fichiers.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <dirent.h>
+#include <sys/stat.h>
 #include <chrono>
 #include <thread>
 #include <unistd.h> // nécessaire pour l'utilisation de la fct access()
@@ -11,17 +13,48 @@ using std::cin;
 using std::endl;
 using std::string;
 using std::getline;
+using namespace std;
 
+void explore(char * dir_name)
+{
+	DIR *dir; //pointer to open directory
+	struct dirent *entry; //stuff inside the direct
+	struct stat info; //information about each entry
+
+ 
+	//1 open
+	dir = opendir(dir_name);
+	if (!dir) {
+		cout << "Directory was not found \n";
+		return;
+	}
+
+	//2read
+	cout << "\nThis folder contains : \n" << endl;
+	while ((entry = readdir(dir)) != NULL)
+	{
+		if (entry->d_name[0] != '.'){
+			string path = string(dir_name) + '/' + string(entry->d_name);
+			cout << "- " << path << '\n' << endl;
+			stat(path.c_str(), &info);
+		}
+	}
+	
+	//3close
+	closedir(dir);
+}
 string init(){
 		string pathname = "";
-		cout << "Where would you save the files ?" << endl;
-		getline(cin, pathname);
-		/*int a = access(pathname, F_OK);
-		while(a == -1){
-				cout << "This repository doesn't exist. Try another address" << endl;
-				getline(cin, pathname);
-				a = access(pathname, F_OK);
-		}*/
+		string uresponse = "";
+		string y = "y";
+		while(uresponse != y){
+			cout << "Where would you save the files ?" << endl;
+			getline(cin, pathname);
+			// EXAMPLE : /mnt/c/Users/Louis Vande Perre/Documents/Polytech/BA3(Biomed)/Informatique/Projet/test/
+			explore((char*) pathname.c_str()); //c_str converti un string en char* (type nécessaire à la bonne exécution de la fonction explore()
+			cout << "Is it a good directory ? (y/n)" << endl;
+			getline(cin, uresponse);
+		}
 		return pathname;
 }
 
@@ -33,16 +66,21 @@ int main(){
 		ofstream fichier_noir(pathname+"/noir.txt");
 		ofstream fichier_blanc(pathname+"/blanc.txt");
 		string to_write = "";
-		cout << "Que voulez vous écrire dans blanc.txt ?" << endl;	// ATTENTION, POUR LE MOMENT NE MARCHE QU'AVEC "." POUR SAUVER DANS LE DOSSIER DANS LEQUEL ON SE TROUVE 
+		cout << "Que voulez vous écrire dans blanc.txt ?" << endl;
 		getline(cin, to_write);
 		if(fichier_blanc.is_open()){
 			fichier_blanc << to_write;
 			fichier_blanc.close();
 		}
+		else
+			cout << "File is not open" << endl;
 		cout << "Que voulez vous écrire dans noir.txt ?" << endl;
 		getline(cin, to_write);
 		if(fichier_noir.is_open()){
 			fichier_noir << to_write;
 			fichier_noir.close();
 		}
+		else
+			cout << "File is not open" << endl;
+			
 }
