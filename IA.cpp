6054b4a7bf70503f51e07c0d269b2253fec89cf8
RRaffoul <rom.raffoul@gmail.com>
2017-12-16 00:35:59 +0100

    Ameliorations et constructeurs + play()

--- IA.cpp
+++ IA.cpp
@@ -1,28 +1,52 @@
 
 
+IA::IA(Plateau* platee, Vue* vuee){
+	plate = platee;
+	vue = vuee;
+}
+
+IA::~IA(){
+	cout << "Destruction d'une IA" << endl;
+}
+
+void IA::Play(int turn){
+	vue->Print_state(plate->Get_Noirs(), plate->Get_Blancs(), turn);
+	plate->Set_Turn(turn);
+	pos = Search_func(&plate);
+	if(pos[0] == 0 && pos [1] == 0){
+		vue->Skip_turn();
+	}
+	else if(plate->Check_eat(pos[0],pos[1])){
+		plate->Eat();
+	}
+}
+
+
+
 
 int IA::A(Plateau board){
-	int best[3] = {0};
+	int best[3] = {0};	
 	if(count <= 5){ //si on s arrete a 4 (pair) ca n a pas de sens, on s arreterait sur un coup d un adversaire, pas sur un a toi
-		if (count % 2 == 0){ //donc c est au tour de l adversaire, ca compte pas ses point mais on pourrait proceder par malus exactement de la meme facon en negatif
-			original = board;
+		if (count % 2 == 0){ /*donc c est au tour de l adversaire, ca compte pas ses point mais on pourrait proceder par malus 
+			exactement de la meme facon en negatif ou des bonus si l adversaire passe son tour ahah !*/
 			imaginaire = board;
 			pos_to_check = imaginaire.Pos_play(); //jsp si ca s ecrti vector1 = vector2 
 			int a = pos_to_check.size(); //pas mieux de faire ca plutot que de le mettre dans le if comme dans plateau ? Doit compter moins de fois tout non ?
-			for (i = 0; i < a; i+=2){
-				imaginaire = original;
+			for (i = 0; i < a; i+=2){  //on remarque que si a = 0 on a un retour de 00 pour le choix de position
+				imaginaire = board;
 				if(imaginaire->Check_eat(pos_to_check(i), pos_to_check(i+1))){//a regler ca hein
 					imaginaire.Eat();
 				}
+				futur [3] = A(imaginaire)
+				best[0] = futur[0];  //ici on devrait remettre valeur etc pour rajouter des points negatifs
 			}
 		}
 		else{ //donc c est a nous
-			original = board;
 			imaginaire = board;
 			pos_to_check = imaginaire.Pos_play(); //jsp si ca s ecrti vector1 = vector2 
 			int a = pos_to_check.size(); //pas mieux de faire ca plutot que de le mettre dans le if comme dans plateau ? Doit compter moins de fois tout non ?
 			for (i = 0; i < a; i+=2){
-				imaginaire = original;
+				imaginaire = board;
 				if(imaginaire->Check_eat(pos_to_check(i), pos_to_check(i+1))){//a regler ca hein
 					imaginaire.Eat();
 				}
--- IA.h
+++ IA.h
@@ -2,11 +2,16 @@
 
 class IA{
  private:
+	Plateau* plate;
+	Vue* vue;
 	Plateau original;
 	Plateau imaginaire;
 	vector<int> pos_to_check;
 	int count;
+	int pos[2]; 
  public:
+	IA();
+	~IA();
 	int Search_func(Plateau board);
 	int A(Plateau board);
 }
